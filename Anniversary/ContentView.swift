f56e4e2970b3be5f0653536818bf4e9575a62e90
yzqi <1370322806@qq.com>
2020-05-28 09:53:33 +0800

    init commit.

---
+++ Anniversary/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Anniversary
+//
+//  Created by Yanzi Qi  on 2020/5/28.
+//  Copyright © 2020 yzqi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
